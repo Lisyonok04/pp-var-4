f76833f6a489938225f16854647bdf37ee9f91c5
Mary <marybelka04@gmail.com>
2023-11-09 10:23:12 +0400

    Update tulipsroses.py

--- tulipsroses.py
+++ tulipsroses.py
@@ -1,40 +1,45 @@
+import requests
 import os
 import shutil
 import time
 
-import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
 
 def make_folder(name: str) -> None:
 
     if not os.path.isdir(name):
         os.mkdir(name)
 
-def pic_links(request: str) -> None:
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+def pic_links(request: str, num: int) -> None:
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()))
     url = f"https://yandex.ru/images/search?text={request}"
     driver.get(url = url)
-    #driver.maximize_window()
-    time.sleep(8)
+    driver.maximize_window()
+    time.sleep(10)
     driver.find_element(
         By.CSS_SELECTOR, 'div.serp-item__preview a.serp-item__link').click()
     with open(f"{request}.txt", 'w') as file:
-        for i in range(10):
+        for i in range(num):
             try:
                 time.sleep(0.5)
                 link = driver.find_element(
-                    By.CSS_SELECTOR, "a.Button2_view_action").get_attribute("href")
+                    By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
                 file.write(link + '\n')
                 driver.find_element(
-                    By.CSS_SELECTOR, "div.CircleButton:nth-child(4)").click()
+                    By.CLASS_NAME, "MediaViewer_theme_fiji-ButtonNext").click()
             except:
                 continue
 
+    
     driver.close()
     driver.quit()
+    
+
 
 def download(request: str) -> None:
     make_folder("dataset")
@@ -47,9 +52,9 @@ def download(request: str) -> None:
                 time.sleep(5)
                 response = requests.get(url, stream=True)
                 if response.status_code == 200:
-                    count += 1
                     with open(f"dataset/{request}/{str(count).zfill(4)}.jpg", "wb") as image_file:
                         shutil.copyfileobj(response.raw, image_file)
+                    count += 1
                 else:
                     continue
             except:
@@ -59,9 +64,10 @@ def main() -> None:
 
     if os.path.isdir("dataset"):
         shutil.rmtree("dataset")
-    #request = "rose"
-    #pic_links(request)
-    #download(request)
-    request = "tulips"
-    pic_links(request)
+    request = "tulip"
+    pic_links(request, 10)
+    download(request)
+
+    request = "rose"
+    pic_links(request, 10)
     download(request)
