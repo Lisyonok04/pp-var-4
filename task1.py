29abd686b76d77411ab08a766802a97a9aa1ab92
Mary <marybelka04@gmail.com>
2023-12-07 10:15:25 +0400

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,7 +5,7 @@ from typing import List
 def write_in_file(name_class: str, number: int) -> None:
 
     with open("dataset.csv", "a", newline='', encoding='utf8') as file:
-        printer = csv.writer(file, delimiter=";")
+        printer = csv.writer(file, delimiter=" ")
         printer.writerow(
             [os.path.abspath(create_relative_way(name_class, number)),
              create_relative_way(name_class, number),
@@ -17,7 +17,7 @@ def create_csv(namecsv: str) -> None:
     num_files = len([f for f in os.listdir(namecsv + "/tulip")
                      if os.path.isfile(os.path.join(namecsv + "/tulip", f))])
     with open("dataset.csv", "w", newline='') as file:
-        printer = csv.writer(file, delimiter=";", )
+        printer = csv.writer(file, delimiter=" ", )
         printer.writerow(["The Absolute Way", "Relative Way", "Class"])
     for i in range(0, num_files):
         name_class = "tulip"
