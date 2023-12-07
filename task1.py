76fdda91e8857a736d8f51ac3cf3e86fcc80eaf7
Mary <marybelka04@gmail.com>
2023-12-07 08:53:40 +0400

    Update task1.py

--- task1.py
+++ task1.py
@@ -14,5 +14,16 @@ def relative_way(name: str, dir: str) -> List[str]:
 
 def create_csv(namecsv: str) -> None:
     {
-        
+
     }
+
+def create_relative_way(name_class: str, number: int) -> str:
+    return f"dataset/download_data/{name_class}/{str(number).zfill(4)}.jpg"
+
+
+def create_another_relative_way(name_class: str, number: int) -> str:
+    return f"dataset/dataset_another/{name_class}_{str(number).zfill(4)}.jpg"
+
+
+def create_number_relative_way(number: int) -> str:
+    return f"dataset/dataset_number/{str(number).zfill(4)}.jpg"
