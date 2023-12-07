9cbec3a044198b8ef57dfb0eb6d8e60f8f9ecbb4
Mary <marybelka04@gmail.com>
2023-12-07 08:50:40 +0400

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,18 @@
+import csv
+import os
+from typing import List
+
+def absolute_way(name: str, dir: str) -> List[str]: 
+    {
+
+    }
+
+def relative_way(name: str, dir: str) -> List[str]:
+    {
+
+    }
+
+def create_csv(namecsv: str) -> None:
+    {
+        
+    }
