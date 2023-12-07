2cce208aa037772bca4157b7c5b72949482b97ca
Mary <marybelka04@gmail.com>
2023-12-07 10:15:28 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,8 +4,8 @@ from typing import List
 
 def write_in_file(name_class: str, number: int) -> None:
 
-    with open("dataset.csv", "a", newline='', encoding='utf8') as file:
-        printer = csv.writer(file, delimiter=";")
+    with open("dataset2.csv", "a", newline='', encoding='utf8') as file:
+        printer = csv.writer(file, delimiter=" ")
         printer.writerow(
             [os.path.abspath(create_another_relative_way(name_class, number)),
              create_another_relative_way(name_class, number),
@@ -16,8 +16,8 @@ def write_in_file(name_class: str, number: int) -> None:
 def create_csv(namecsv: str) -> None:
     num_files = len([f for f in os.listdir(namecsv + "/tulip")
                      if os.path.isfile(os.path.join(namecsv + "/tulip", f))])
-    with open("dataset.csv", "w", newline='') as file:
-        printer = csv.writer(file, delimiter=";", )
+    with open("dataset2.csv", "w", newline='') as file:
+        printer = csv.writer(file, delimiter=" ", )
         printer.writerow(["The Absolute Way", "Relative Way", "Class"])
     for i in range(0, num_files):
         name_class = "tulip"
@@ -30,7 +30,7 @@ def create_csv(namecsv: str) -> None:
             write_in_file(name_class, i)
 
 def create_another_relative_way(name_class: str, number: int) -> str:
-    return f"dataset/{name_class}_{str(number).zfill(4)}.jpg"
+    return f"dataset2/{name_class}_{str(number).zfill(4)}.jpg"
 
 if __name__ == "__main__":
-    create_csv("dataset")
+    create_csv("dataset2")
