fbb73d0223ccc36cca9d96330a630fc8f03e55a0
Mary <marybelka04@gmail.com>
2023-12-16 18:47:54 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -10,9 +10,9 @@ def copy_in_file(old: str, new: str, name: str) -> None:
     list_images: name = os.listdir(path)
     for img in list_images:
         shutil.copy(os.path.join(path, img), os.path.join(new, f"{name}_{img}"))
-        with open("Annotation2.csv", "a") as f:
+        with open("Annotasion2.csv", "a") as f:
             printer = csv.writer(f, delimiter=" ")
-            printer.writerow(["The Absolute Way", "Relative Way", "Class"])
+            #printer.writerow(["The Absolute Way", "Relative Way", "Class"])
             printer.writerow(
                 [
                     os.path.join(abs_path, f"{name}_{img}"),
@@ -21,17 +21,21 @@ def copy_in_file(old: str, new: str, name: str) -> None:
                 ]
             )
     
+def creating_csvfile(namecsv: str) -> None:
+    """
+    The function takes as input the name for the .csv file,
+    creates a .csv file with the passed name and writes the column headers.
+    """
+    with open("Annotasion2.csv", "w", newline="") as f:
+        filewriter = csv.writer(f, delimiter=" ", lineterminator="\r")
+        filewriter.writerow(["Absolute path", "Relative path", "Class name"])
 
-#def create_csv(namecsv: str) -> None:
- #   with open("Annotasion2.csv", "w", newline='') as file:
-  #      printer = csv.writer(file, delimiter=" ", )
-   #     printer.writerow(["The Absolute Way", "Relative Way", "Class"])
 
 
 def create_another_relative_way(name_class: str, number: int) -> str:
     return f"dataset2/{name_class}_{str(number).zfill(4)}.jpg"
 
 if __name__ == "__main__":
-    #create_csv("Annotasion2")
+    creating_csvfile("Annotasion2.csv")
     copy_in_file("dataset", "dataset2", "tulip")
     copy_in_file("dataset", "dataset2", "rose")
