4258fe3820e67c21d6ebced514f93a88c58a99be
Mary <marybelka04@gmail.com>
2023-12-16 14:49:35 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,36 +1,37 @@
 import csv
+import shutil
 import os
 from typing import List
 
-def write_in_file(name_class: str, number: int) -> None:
+def copy_in_file(old: str, new: str, name: str) -> None:
+    abs_path: str = os.path.abspath(new)
+    rel_path: str = os.path.relpath(new)
+    path: str = os.path.join(os.path.abspath(old), name)
+    list_images: name = os.listdir(path)
+    for img in list_images:
+        shutil.copy(os.path.join(path, img), os.path.join(new, f"{name}_{img}"))
+        with open("Annotation2.csv", "a") as f:
+            printer = csv.writer(f, delimiter=" ")
+            printer.writerow(["The Absolute Way", "Relative Way", "Class"])
+            printer.writerow(
+                [
+                    os.path.join(abs_path, f"{name}_{img}"),
+                    os.path.join(rel_path, f"{name}_{img}"),
+                    name,
+                ]
+            )
+    
 
-    with open("dataset2.csv", "a", newline='', encoding='utf8') as file:
-        printer = csv.writer(file, delimiter=" ")
-        printer.writerow(
-            [os.path.abspath(create_another_relative_way(name_class, number)),
-             create_another_relative_way(name_class, number),
-             name_class]
-        )
+#def create_csv(namecsv: str) -> None:
+ #   with open("Annotasion2.csv", "w", newline='') as file:
+  #      printer = csv.writer(file, delimiter=" ", )
+   #     printer.writerow(["The Absolute Way", "Relative Way", "Class"])
 
-
-def create_csv(namecsv: str) -> None:
-    num_files = len([f for f in os.listdir(namecsv + "/tulip")
-                     if os.path.isfile(os.path.join(namecsv + "/tulip", f))])
-    with open("dataset2.csv", "w", newline='') as file:
-        printer = csv.writer(file, delimiter=" ", )
-        printer.writerow(["The Absolute Way", "Relative Way", "Class"])
-    for i in range(0, num_files):
-        name_class = "tulip"
-        way = f"{namecsv}/{name_class}/{str(i).zfill(4)}.jpg"
-        if os.path.isfile(way):
-            write_in_file(name_class, i)
-        name_class = "rose"
-        way = f"{namecsv}/{name_class}/{str(i).zfill(4)}.jpg"
-        if os.path.isfile(way):
-            write_in_file(name_class, i)
 
 def create_another_relative_way(name_class: str, number: int) -> str:
     return f"dataset2/{name_class}_{str(number).zfill(4)}.jpg"
 
 if __name__ == "__main__":
-    create_csv("dataset2")
+    #create_csv("Annotasion2")
+    copy_in_file("dataset", "dataset2", "tulip")
+    copy_in_file("dataset", "dataset2", "rose")
