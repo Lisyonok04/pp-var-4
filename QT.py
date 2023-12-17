5925b7f9eea8204673f727b329adc088a491a32e
Mary <marybelka04@gmail.com>
2023-12-17 15:35:33 +0400

    Update QT.py

--- QT.py
+++ QT.py
@@ -4,7 +4,7 @@ from typing import List
 from PyQt5 import QtCore
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QFont
 from PyQt5.QtWidgets import (
     QApplication,
     QDialog,
@@ -30,7 +30,7 @@ class Interface(QWidget):
                                         background: rgb(127, 255, 212);
                                         border-style: outset; 
                                         border-width: 5px;
-                                        font-size: 24px;
+                                        font-size: 20px;
                                          ''')
         self.buttonSelect.setFixedSize(500, 60)
 
@@ -39,9 +39,46 @@ class Interface(QWidget):
                                         background: rgb(102, 205, 170);
                                         border-style: outset; 
                                         border-width: 5px;
-                                        font-size: 24px;
+                                        font-size: 20px;
                                         ''')
         self.buttonCreate.setFixedSize(500, 60)
+
+        self.buttonNew = QPushButton("Create new dataset and annotasion for dataset", self)
+        self.buttonNew.setStyleSheet('''
+                                        background: rgb(64, 224, 208);
+                                        border-style: outset; 
+                                        border-width: 5px;
+                                        font-size: 20px;
+                                        ''')
+        self.buttonNew.setFixedSize(500, 60)
+
+        self.buttonRandom = QPushButton("Create new random dataset and annotasion for dataset", self)
+        self.buttonRandom.setStyleSheet('''
+                                        background: rgb(72, 209, 204);
+                                        border-style: outset; 
+                                        border-width: 5px;
+                                        font-size: 19px;
+                                        ''')
+        self.buttonRandom.setFixedSize(500, 60)
+
+        self.buttonRose = QPushButton("Next rose", self)
+        self.buttonRose.setStyleSheet('''
+                                        background: rgb(0, 206, 209);
+                                        border-style: outset; 
+                                        border-width: 5px;
+                                        font-size: 19px;
+                                        ''')
+        self.buttonRose.setFixedSize(500, 60)
+
+        self.buttonTulip = QPushButton("Next tulip", self)
+        self.buttonTulip.setStyleSheet('''
+                                        background: rgb(0, 255, 255);
+                                        border-style: outset; 
+                                        border-width: 5px;
+                                        font-size: 19px;
+                                        ''')
+        self.buttonTulip.setFixedSize(500, 60)
+
 
 def main() -> None:
     """
